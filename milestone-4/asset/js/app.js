df2d3268eb429ec17f1c1d54291bd9355e34b10f
gaianacheli <gaianacheli04@gmail.com>
2021-11-04 15:30:31 +0100

    mileston 4 preparato

---
+++ milestone-4/asset/js/app.js
@@ -0,0 +1,119 @@
+const app = new Vue ({
+    el:"#app",
+    data: {
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: './asset/img/avatar_1.jpg',
+                visible: true,
+                messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Ricordati di dargli da mangiare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            text: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                      ],
+            },
+            {
+                name: 'Fabio',
+                avatar: './asset/img/avatar_2.jpg',
+                visible: true,
+                messages: [
+                            {
+                                date: '20/03/2020 16:30:00',
+                                text: 'Ciao come stai?',
+                                status: 'sent'
+                            },
+                            {
+                                date: '20/03/2020 16:30:55',
+                                text: 'Bene grazie! Stasera ci vediamo?',
+                                status: 'received'
+                            },
+                            {
+                                date: '20/03/2020 16:35:00',
+                                text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                                status: 'sent'
+                            }
+                          ],
+             },
+            {
+                name: 'Samuele',
+                avatar: './asset/img/avatar_3.jpg',
+                visible: true,
+                messages: [
+                            {
+                                date: '28/03/2020 10:10:40',
+                                text: 'La Marianna va in campagna',
+                                status: 'received'
+                            },
+                            {
+                                date: '28/03/2020 10:20:10',
+                                text: 'Sicuro di non aver sbagliato chat?',
+                                status: 'sent'
+                            },
+                            {
+                                date: '28/03/2020 16:15:22',
+                                text: 'Ah scusa!',
+                                status: 'received'
+                            }
+                          ],
+            },
+            {
+                name: 'Luisa',
+                avatar: './asset/img/avatar_4.jpg',
+                visible: true,
+                messages: [
+                            {
+                                date: '10/01/2020 15:30:55',
+                                text: 'Lo sai che ha aperto una nuova pizzeria?',
+                                status: 'sent',
+                            },
+                            {
+                                date: '10/01/2020 15:50:00',
+                                text: 'Si, ma preferirei andare al cinema',
+                                status: 'received'
+                            }
+                          ],
+                         
+            },
+            ],
+            counter: 0,
+            newMessages: '',
+            
+    },
+    methods: {
+        showChat(index) {
+            this.counter = index
+        },
+        addMessage(){
+            if (this.newMessages != '')
+            this.contacts[this.counter].messages.push({
+                date:new Date().toLocaleString(),
+                text: this.newMessages,
+                status: 'sent'
+            })  
+            setTimeout(() =>{
+                this.contacts[this.counter].messages.push({
+                 date: new Date().toLocaleString(),
+                 text: 'ok',
+                 status: 'received',
+                });
+             
+            },1000) 
+         
+            this.newMessages=''
+        },
+
+
+    }
+})
